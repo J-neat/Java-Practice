9bca056e0653918d7cac2c05358b543d0dbb57fa
J-neat <junghyo0258@catholic.ac.kr>
2024-09-04 10:11:00 +0900

    Feat: IfElseExample

---
+++ src/chap004/IfElseExample.java
@@ -0,0 +1,17 @@
+package chap004;
+
+public class IfElseExample {
+    public static void main(String[] args) {
+        int score = 93;
+
+        if (score >= 90) {
+            System.out.println("등급은 A입니다");
+        } else if (score >= 80 && score < 90) {
+            System.out.println("등급은 B입니다");
+        } else if (score >= 70 && score < 80) {
+            System.out.println("등급은 C입니다");
+        } else {
+            System.out.println("등급은 D입니다");
+        }
+    }
+}
