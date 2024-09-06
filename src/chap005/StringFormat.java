ad3914edb2add0b9f6c39c86754eb01b28ff4690
J-neat <junghyo0258@catholic.ac.kr>
2024-09-06 09:47:46 +0900

    Chore: StringFormat 예시 추가

--- src/chap005/StringFormat.java
+++ src/chap005/StringFormat.java
@@ -3,10 +3,15 @@ package chap005;
 public class StringFormat {
 
     public static void main(String[] args){
-        String str2 = "치환값1: %d .... 치환괎2:  %d";
+        String str = ".........%s....특정문자%s사이값";
+        System.out.println(String.format(str, "치환값1", "치환값2"));
+        System.out.println(String.format(str, 4, 5));
+        System.out.println(String.format(str, 46567.785, 5.234235));
+
+        String str2 = "치환값1: %d .....치환값2: %d";
         System.out.println(String.format(str2, 7, 8));
 
-        String str3 = "치환값1: % f .... 치환괎2:  %f";
-        System.out.println(String.format(str3, 4.5, 6.7));
+        String str3 = "치환값1: %.18f .....치환값2: %.10f";
+        System.out.println(String.format(str3, 4.5438972598439284, 6.7));
     }
 }
