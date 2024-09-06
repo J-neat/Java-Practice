2b39b17b3cc6927532982d1ee3ee34e755310850
J-neat <junghyo0258@catholic.ac.kr>
2024-09-06 09:47:34 +0900

    Chore: StringBuilderBufferExample 예시 추가

--- src/chap005/StringBuilderBufferExample.java
+++ src/chap005/StringBuilderBufferExample.java
@@ -3,23 +3,32 @@ package chap005;
 public class StringBuilderBufferExample {
     public static void main(String[] args){
         String result = "";
-        result += "Hello  ";
-        result += "Java  ";
-        result += "World!  ";
+        result += "Hello ";
+        result += "Java ";
+        result += "World!";
         System.out.println(result);
 
         StringBuffer buffer = new StringBuffer();
-        buffer.append("Hello  ");
-        buffer.append("Java  ");
-        buffer.append("World!  ");
+        buffer.append("Hello ");
+        buffer.append("Java ");
+        buffer.append("World!");
         System.out.println(buffer);
 
         StringBuilder builder = new StringBuilder();
-        builder.append("Hello  ");
-        builder.append("Java  ");
-        builder.append("World!  ");
+        builder.append("Hello ");
+        builder.append("Java ");
+        builder.append("World!");
         System.out.println(builder);
 
-//        StringBuffer, Builder 각각 insert() substring() 메소드 호출해보기
+        // insert()
+        System.out.println(builder.insert(0, "첫번째"));
+        System.out.println(builder.insert(builder.length(), "오십번째다"));
+
+        // substring()
+        String substringResult = buffer.substring(6);       // Java World!
+        String substringResult2 = buffer.substring(6, 7);       // J
+
+        System.out.println(substringResult);
+        System.out.println(substringResult2);
     }
 }
