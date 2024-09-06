451dbebdbd0193e427e1b1a3c88b515be903dd33
J-neat <junghyo0258@catholic.ac.kr>
2024-09-06 09:47:11 +0900

    Feat: StringNullPointerTestSample

---
+++ src/chap005/StringNullPointerTestSample.java
@@ -0,0 +1,8 @@
+package chap005;
+
+public class StringNullPointerTestSample {
+    public static void main(String[] args) {
+        String str = null;
+        System.out.println(str.equals("비교하려는값"));
+    }
+}
